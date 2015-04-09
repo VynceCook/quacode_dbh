b228675e389fef09109048adcc2ac432ac7aac35
Vincent Barichard <Vincent.Barichard@univ-angers.fr>
2015-04-09 10:47:47 +0200

    Subtree mise en place .....

--- quacode/privateMisc/queso_pb/runit.sh
+++
@@ -1,6 +0,0 @@
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:.
-
-java -Xmx500M -server -cp lib/trove.jar:classes/ queso.experiments.connectfourfullor2 $@
-#java -Xmx500M -server -cp lib/trove.jar:classes/ queso.experiments.bakertest $@
-#java -Xmx500M -server -cp lib/trove.jar:classes/ queso.experiments.nimfibo $@
-#java -Xmx500M -server -cp lib/trove.jar:classes/ queso.experiments.matrixgame $@
