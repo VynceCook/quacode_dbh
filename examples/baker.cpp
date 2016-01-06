5e381507af5a0ce29cca1b37061bf002a462592d
goldensuneur <goldensuneur@gmail.com>
2016-01-06 15:38:34 +0100

    Improves Baker

--- examples/baker.cpp
+++ examples/baker.cpp
@@ -71,30 +71,46 @@ public:
 
     /// Print strategy or not
     Gecode::Driver::BoolOption _printStrategy;
-    int n; /// Parameter to be given on the command line
+    //Size of the domains
+    Gecode::Driver::UnsignedIntOption _n;
+    //Algorithm to use
+    Gecode::Driver::UnsignedIntOption _algo;
     /// Initialize options for example with name \a s
     BakerOptions(const char* s, int n0)
         : Options(s),
         _printStrategy("-printStrategy","Print strategy",false),
-        n(n0) {
+        _n("-n", "Value used to restrict the domain of w1 in order to make the problem harder", n0),
+        _algo("-algo", "Number of the algorithm to use : 0 - Logger, 1 - Montecarlo, 2 - GenAlgo", 0)
+        {
             add(_printStrategy);
+            add(_n);
+            add(_algo);
         }
+    ///Destructor to free the algorithm pointer
+    ~BakerOptions() {
+        if (aAlgo) {
+            delete aAlgo;
+        }
+    }
     /// Parse options from arguments \a argv (number is \a argc)
     void parse(int& argc, char* argv[]) {
         Options::parse(argc,argv);
-        if (argc < 2)
-            return;
-        n = atoi(argv[1]);
     }
     /// Return true if the strategy must be printed
     bool printStrategy(void) const {
         return _printStrategy.value();
     }
+    ///Return n
+    unsigned int n() const {
+        return _n.value();
+    }
+    ///Return the algorithm
+    unsigned int algo() const {
+        return _algo.value();
+    }
     /// Print help message
     virtual void help(void) {
         Options::help();
-        std::cerr << "\t(unsigned int) default: " << n << std::endl
-            << "\t\tValue used to restrict the domain of w1 in order to make the problem harder" << std::endl;
     }
 };
 
@@ -109,32 +125,32 @@ public:
         if (!opt.printStrategy()) strategyMethod(0); // disable build and print strategy
 
         using namespace Int;
-        aAlgo.newVar(EXISTS,"w1",TYPE_INT,1,opt.n);
-        aAlgo.newVar(EXISTS,"w2",TYPE_INT,1,opt.n);
-        aAlgo.newVar(EXISTS,"w3",TYPE_INT,1,opt.n);
-        aAlgo.newVar(EXISTS,"w4",TYPE_INT,1,opt.n);
-        aAlgo.newVar(EXISTS,"w5",TYPE_INT,1,opt.n);
-        aAlgo.newVar(FORALL,"f",TYPE_INT,1,opt.n);
+        aAlgo.newVar(EXISTS,"w1",TYPE_INT,1,opt.n());
+        aAlgo.newVar(EXISTS,"w2",TYPE_INT,1,opt.n());
+        aAlgo.newVar(EXISTS,"w3",TYPE_INT,1,opt.n());
+        aAlgo.newVar(EXISTS,"w4",TYPE_INT,1,opt.n());
+        aAlgo.newVar(EXISTS,"w5",TYPE_INT,1,opt.n());
+        aAlgo.newVar(FORALL,"f",TYPE_INT,1,opt.n());
         aAlgo.newVar(EXISTS,"c1",TYPE_INT,-1,1);
         aAlgo.newVar(EXISTS,"c2",TYPE_INT,-1,1);
         aAlgo.newVar(EXISTS,"c3",TYPE_INT,-1,1);
         aAlgo.newVar(EXISTS,"c4",TYPE_INT,-1,1);
         aAlgo.newVar(EXISTS,"c5",TYPE_INT,-1,1);
-        aAlgo.newAuxVar("o1",TYPE_INT,-opt.n,opt.n);
-        aAlgo.newAuxVar("o2",TYPE_INT,-opt.n,opt.n);
-        aAlgo.newAuxVar("o3",TYPE_INT,-opt.n,opt.n);
-        aAlgo.newAuxVar("o4",TYPE_INT,-opt.n,opt.n);
-        aAlgo.newAuxVar("o5",TYPE_INT,-opt.n,opt.n);
+        aAlgo.newAuxVar("o1",TYPE_INT,-opt.n(),opt.n());
+        aAlgo.newAuxVar("o2",TYPE_INT,-opt.n(),opt.n());
+        aAlgo.newAuxVar("o3",TYPE_INT,-opt.n(),opt.n());
+        aAlgo.newAuxVar("o4",TYPE_INT,-opt.n(),opt.n());
+        aAlgo.newAuxVar("o5",TYPE_INT,-opt.n(),opt.n());
 
-        IntVarArgs w(*this,5,1,opt.n);
-        IntVar f(*this,1,opt.n);
+        IntVarArgs w(*this,5,1,opt.n());
+        IntVar f(*this,1,opt.n());
         setForAll(*this, f);
         IntVarArgs c(*this,5,-1,1);
         IntVarArgs vaX;
         vaX << w << f << c;
         X = IntVarArray(*this, vaX);
 
-        IntVar o1(*this,-opt.n,opt.n), o2(*this,-opt.n,opt.n), o3(*this,-opt.n,opt.n), o4(*this,-opt.n,opt.n), o5(*this,-opt.n,opt.n);
+        IntVar o1(*this,-opt.n(),opt.n()), o2(*this,-opt.n(),opt.n()), o3(*this,-opt.n(),opt.n()), o4(*this,-opt.n(),opt.n()), o5(*this,-opt.n(),opt.n());
         rel(*this, w[0] * c[0] == o1);
         rel(*this, w[1] * c[1] == o2);
         rel(*this, w[2] * c[2] == o3);
@@ -181,10 +197,21 @@ int main(int argc, char* argv[])
     BakerOptions opt("Baker Problem",40);
     opt.parse(argc,argv);
 
-    // Logger aAlgo;
-    // DumbAlgorithm aAlgo;
-    GenAlgo aAlgo;
-    opt.aAlgo = &aAlgo;
+    switch (opt.algo()) {
+        default:
+        case 0:
+            opt.aAlgo = new Logger();
+            break;
+
+        case 1:
+            opt.aAlgo = new MonteCarlo();
+            break;
+
+        case 2:
+            opt.aAlgo = new GenAlgo();
+            break;
+    }
+
     Script::run<QCSPBaker,QDFS,BakerOptions>(opt);
 
     return 0;
